d3db421cfd6b1c663d199a6e5e7cd821b065480b
KingNasirGulp <matteodanelli07@gmail.com>
2026-01-15 13:39:27 +0000

    seconda prova

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,29 +1,48 @@
-//import { Link } from "react-router";
-
 const cardStyle =
-"bg-green-500 w-40 h-48 text-center flex flex-col items-center justify-center";
+  "bg-green-500 w-40 h-48 text-center flex flex-col items-center justify-center rounded-lg";
 
-const PokemonCard = ({ name, image }: { name: string; image: string }) => {
-return (
-<div className={cardStyle}>
-<img src={image} alt={name} className="w-30 h-30 mb-2" />
-<strong className="text-white">{name}</strong>
-</div>
-);
+type PokemonCardProps = {
+  name: string;
+  image: string;
+};
+
+const PokemonCard = ({ name, image }: PokemonCardProps) => {
+  return (
+    <div className={cardStyle}>
+      <img src={image} alt={name} className="w-24 h-24 mb-2" />
+      <strong className="text-white">{name}</strong>
+    </div>
+  );
 };
 
 export const Root = () => {
-const pokemons = [
-{ name: "Pikachu", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" },
-{ name: "Bulbasaur", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" },
-{ name: "Charmander", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" },
-];
+  const pokemons = [
+    {
+      name: "Pikachu",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    },
+    {
+      name: "Bulbasaur",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    },
+    {
+      name: "Charmander",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    },
+  ];
 
-return (
-<div className="space-x-2 flex flex-wrap justify-center">
-{pokemons.map((Pokemon) => (
-<PokemonCard key={Pokemon.name} name={Pokemon.name} image={Pokemon.image} />
-))}
-</div>
-);
+  return (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {pokemons.map((pokemon) => (
+        <PokemonCard
+          key={pokemon.name}
+          name={pokemon.name}
+          image={pokemon.image}
+        />
+      ))}
+    </div>
+  );
 };
